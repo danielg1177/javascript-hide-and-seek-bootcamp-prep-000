873ff072ea0e22d0eff0094afd67bbc2cc800bbe
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-16 15:57:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   let list = document.querySelectorAll('.ranked-list')
   
   for(let i = 0; list.length > i; i++) {
-    let test = list.children[i]
+    let test = list[i].children
     
     for(let j = 0; test.length > j; j++) {
       test[j].innerHTML = parseInt(test[j].innerHTML) + n
