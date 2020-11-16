cc64f29a3d746311f5b6cf8a5c747772fdc93b1b
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-16 15:51:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,32 +2,6 @@ function getFirstSelector(select) {
   return document.querySelector(select)
 }
 
-function nestedTarget(target, nested) {
+function nestedTarget() {
   return document.querySelector('#nested .target')
-}
-
-function increaseRankBy(n) {
-  let list = document.querySelectorAll('.ranked-list')
-  
-  for(let i = 0; list.length > i; i++) {
-    
-    let test = list[i].children
-    
-    for(let j = 0; test.length > j; j++) {
-      test[j].innerHTML = parseInt(test[j].innerHTML) + n
-    }
-  }
-}
-
-function deepestChild() {
-  
-  let start = document.getElementById('grand-node')
-  let next = start.children[0]
-  
-  while(next) {
-    
-    start = next
-    next = start.children[0]
-  }
-  return start
 }
