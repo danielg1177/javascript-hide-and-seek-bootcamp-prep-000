055291d2e4c8c025df36cc6a1a25f675a1e6fe88
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-16 15:40:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n) {
     let test = list[i].children
     
     for(let j = 0; test.length > j; j++) {
-      test = 
+      test[j].innerHTML = parseInt(test[j]) + n
     }
   }
 }
